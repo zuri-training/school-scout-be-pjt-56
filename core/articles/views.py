82651e6c512135f351bfcd116bbf120d3b8fd10b
pauline-banye <paulinebanye@gmail.com>
2021-07-03 16:29:14 +0100

    permissions added

--- core/articles/views.py
+++ core/articles/views.py
@@ -1,38 +1,39 @@
-from rest_framework.generics import (
-    ListAPIView, 
-    RetrieveAPIView, 
-    CreateAPIView, 
-    UpdateAPIView, 
-    DestroyAPIView
-)
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser 
 
-from .serializers import ArticleSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from core.models import Article
+from .serializers import ArticleSerializer
 
 
-class ArticleListAPIView(ListAPIView):
+class ArticleList(ListCreateAPIView):
+    
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
-class ArticleDetailAPIView(RetrieveAPIView):
+
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
+
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = 'slug'
     lookup_url_kwarg = 'slug'
 
-class ArticleCreateAPIView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleUpdateAPIView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    lookup_url_kwarg = 'slug'
-
-
-class ArticleDestroyAPIView(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    lookup_url_kwarg = 'slug'
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+    
