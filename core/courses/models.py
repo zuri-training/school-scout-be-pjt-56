8e3bf9c9b0fc56aaa396b220988f82abb60136cd
admin <bassahezekiah@gmail.com>
2021-07-10 15:24:42 +0100

    Update Models

--- core/courses/models.py
+++ core/courses/models.py
@@ -1,5 +1,5 @@
 from core.programs.models import Program
-#from core.department.models import Department
+from core.department.models import Department
 from django.db import models
 from autoslug import AutoSlugField
 from core.schools.models import School
@@ -17,7 +17,7 @@ class Course(models.Model):
     image = models.FileField(upload_to=None, null=True, blank=True)
     course_requirements = models.TextField()
     program = models.ForeignKey(Program, related_name='courses', on_delete=models.CASCADE, null=True, blank=True)
-    #name_of_department = models.ForeignKey(Department, related_name="department", on_delete=models.CASCADE)
+    name_of_department = models.ForeignKey(Department, related_name="department", on_delete=models.CASCADE)
     school_location = models.CharField(max_length=200, null=True, blank=True, default='')
     bookmarkIcon = models.BooleanField(default=False)
     tuition = models.CharField(max_length=200, null=True, blank=True, default='')
